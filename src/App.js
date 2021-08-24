714fede3d6e7435f353d046e2242367dcdc5a71b
SDxBacon <z141134553@gmail.com>
2021-08-24 14:25:24 +0800

    feat: 🎸 補個Homepage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import Jet from "pages/Jet";
 import Town from "pages/Town";
+import Homepage from "pages/Homepage";
 import CardFire from "pages/CardFire";
 import "./App.css";
 
+const history = createBrowserHistory();
+
 const App = () => {
   return (
-    <div className="App-header">
-      <Router>
-        <Switch>
-          <Route path="/jet" component={Jet} />
+    <Router history={history}>
+      <Switch>
+        <Route path="/jet" component={Jet} />
 
-          <Route path="/town" component={Town} />
+        <Route path="/town" component={Town} />
 
-          <Route path="/card-fire" component={CardFire} />
-        </Switch>
-      </Router>
-    </div>
+        <Route path="/card-fire" component={CardFire} />
+
+        {/* FALLBACK: Homepage */}
+        <Route component={Homepage} />
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/pages/Homepage/Hompage.js
@@ -0,0 +1,20 @@
+const HomePage = () => {
+  return (
+    <div className="App-header">
+      <ul>
+        <li>
+          <a href="/town">Town DEMO</a>
+        </li>
+        <li>
+          <a href="/card-fire">Card Fire DEMO</a>
+        </li>
+
+        <li>
+          <a href="/jet">Jet Fighter DEMO</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default HomePage;
